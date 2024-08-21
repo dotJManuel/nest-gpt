5fa3ec4f84809b6aa1c361ee4606fbc9e0c14aaa
dotJManuel <dotJManuel@outlook.com>
2024-08-21 14:34:44 -0600

    add chat gpt messages

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -9,10 +9,38 @@ export const orthographyCheckUseCase = async( openai: OpenAI, options: Options )
   const { prompt } = options;
 
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [
+      {
+        role: "system",
+        content: `
+					Te serán proveidos textos en español con posibles errores ortográficos y gramaticales,
+					Las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+					Debes de responder en formato JSON,
+					tu tarea es corregirlos y retornar información soluciones,
+					también debes de dar un porcentaje de acierto por el usuario,
+					Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+					Ejemplo de salida:
+					{
+						userScore: number,
+						errors: string[], // ['error -> solución']
+						message: string, // Usa emojis y texto para felicitar al usuario
+					}
+        `
+      },
+      {
+        role: "user",
+				content: prompt
+      }
+    ],
     model: "gpt-4o-mini",
+    temperature: 0.3,
+    max_tokens: 100,
+		// response_format: {
+		// 	type: 'json_object'
+		// }
   });
-
-  return completion.choices[0];
-
+  console.log(completion);
+	const jsonResp = JSON.parse(completion.choices[0].message.content);
+  return jsonResp;
 }
